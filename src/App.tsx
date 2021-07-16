ea6510fec3ca0c8c12888cc5016f77eb23f6cc9e
Manoel Duran <manoel.duran@hotmail.com>
2021-07-16 02:17:41 -0300

    server json

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Provider } from 'react-redux';
+import  Catalog  from './components/Catalog';
+import store from './store';
+
+function App() {
+  return (
+    <Provider store={store}>
+      <Catalog />
+    </Provider>
+  );
+}
+
+export default App;
---
+++ src/store/modules/cart/types.ts
@@ -0,0 +1,14 @@
+export interface IProduct {
+  id: number;
+  title: string;
+  price: number;
+}
+
+export interface ICartItem {
+  product: IProduct[];
+  quantify: number;
+}
+
+export interface ICartState {
+  items: ICartItem[];
+}
